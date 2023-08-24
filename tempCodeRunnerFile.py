37c4aa20e3f3d41fc2c57b1c7d4f06713d46af4c
Akash sg <118187023+akash247777@users.noreply.github.com>
2023-08-24 12:05:05 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+
+                print(f'{deptno1} Department No is not valid \n Please enter a valid department no !')
+        except:
+            print('Enter no not characters!')
+    
+    #Asking to enter the name
+    name1 = input('Please enter the name\n')
+    name=''
+    for i in name1: 
